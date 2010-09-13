c43ab7ee3d09353b779a7c8a93d85c30cc1625c1
Randal Truong <randal.truong@gmail.com>
2010-09-12 17:55:55 -0700

    Initial commit of default Rails 3.0 project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RandalOnRails::Application.config.secret_token = 'fe299b70e493d01011342c08b47bca976dfb76a23db0de048badf5b24333ea2281601f30c2c5501be610341c96f131cf0a7c13e29854a05ddf349fe8aaf9c5de'
